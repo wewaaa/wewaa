50757578f2d53af5b4dee77421ac01ffe0be6efd
kim1387 <kevink0@naver.com>
2022-01-07 01:39:19 +0900

    feat: add AuthController format

---
+++ backend/src/main/java/com/wewaa/backend/social/api/AuthController.java
@@ -0,0 +1,28 @@
+package com.wewaa.backend.social.api;
+
+import com.wewaa.backend.social.model.AuthToken;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthToken> login() {
+        return ResponseEntity.ok(null);
+    }
+
+    @GetMapping("/refresh")
+    public ResponseEntity<AuthToken> refreshToken (HttpServletRequest request, HttpServletResponse response) {
+        return ResponseEntity.ok(null);
+    }
+}

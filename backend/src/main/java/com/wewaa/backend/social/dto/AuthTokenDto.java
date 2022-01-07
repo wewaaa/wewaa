7433a11921e698bb58cb19fdfed755a6ecf64984
kim1387 <kevink0@naver.com>
2022-01-07 15:26:14 +0900

    feat: add AuthTokenDto.java for Token
    make two types AuthToken function

---
+++ backend/src/main/java/com/wewaa/backend/social/dto/AuthTokenDto.java
@@ -0,0 +1,50 @@
+package com.wewaa.backend.social.dto;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.security.Key;
+import java.util.Date;
+
+@Slf4j
+@RequiredArgsConstructor
+public class AuthTokenDto {
+
+    @Getter
+    private final String token;
+    private final Key key;
+
+    //Token 체크시 필요한 암호 키
+    private static final String AUTHORITIES_KEY = "role";
+
+    AuthTokenDto(String id, Date expiry, Key key) {
+        this.key = key;
+        this.token = createAuthToken(id, expiry);
+    }
+
+    AuthTokenDto(String id, String role, Date expiry, Key key) {
+        this.key = key;
+        this.token = createAuthToken(id, role, expiry);
+    }
+
+    private String createAuthToken(String id, Date expiry) {
+        return Jwts.builder()
+                .setSubject(id)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .setExpiration(expiry)
+                .compact();
+    }
+
+    private String createAuthToken(String id, String role, Date expiry) {
+        return Jwts.builder()
+                .setSubject(id)
+                .claim(AUTHORITIES_KEY, role)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .setExpiration(expiry)
+                .compact();
+    }
+
+}

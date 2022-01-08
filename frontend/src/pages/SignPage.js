aa837ceeca73c15ae3d28cb4845b68b0bb3073a4
Yujaebin <dbwpqls9998@naver.com>
2022-01-08 18:34:42 +0900

    feat : SignPage (100%)

--- frontend/src/pages/SignPage.js
+++ frontend/src/pages/SignPage.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import styled from 'styled-components';
 import Button from '@material-ui/core/Button';
-import TextField from './TextField';
-
+import TextField from '@material-ui/core/TextField';
 
 // header를 어떻게 하나의 컴퍼넌트로 정리할지 공부해야됨
 const Fix=styled.div`
@@ -36,25 +35,113 @@ const Home=styled.div`
     font-size: 24px;
     line-height: 36px;
 `;
+//여기까지 헤더로 사용
+const FirstNameLoc=styled.div`
+    position: absolute;
+    width: 335px;
+    left: 473px;
+    top: 275px;
+    float: right;
+`;
+const LastNameLoc=styled.div`
+    position: absolute;
+    width: 281px;
+    left: 850px;
+    top: 275px;
+`;
+const UserIdLoc=styled.div`
+    position: absolute;
+    width: 430px;
+    left: 473px;
+    top: 372px;
+`;
+const PasswordLoc=styled.div`
+    position: absolute;
+    width: 430px;
+    height: 55px;
+    left: 473px;
+    top: 467px;
+`;
+const ConfirmPasswordLoc=styled.div`
+    position: absolute;
+    width: 430px;
+    height: 55px;
+    left: 473px;
+    top: 562px;
+`;
+const EmailAddressLoc=styled.div`
+    position: absolute;
+    width: 430px;
+    height: 58px;
+    left: 473px;
+    top: 657px;
+`; 
+
+
 //업로드 페이지의 세이브로케이션과 하나의 컴포넌트로 묶는 방법을 공부해야함
 const SaveLocation2=styled.div` 
     position: absolute;
     width: 286px;
     height: 58px;
     left: 613px;
-    top: 856px;
+    top: 770px;
 `
+const ConfirmLoc=styled.div`
+    position: absolute;
+    width: 186px;
+    height: 58px;
+    left: 900px;
+    top: 380px;
+`;
+const CommentLoc=styled.div`
+    position: absolute;
+    width: 200px;
+    height: 57px;
+    left: 930px;
+    top: 470px;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 11px;
+    line-height: 15px;
+    color: #000000;
+`; 
+
+
 const SignPage = () => {
     return (
         <Fix>
             <Logo>LOGO</Logo>
             <Home>HOME</Home>
-            <SaveLocation2>
-                <Button variant="contained" color="secondary">Save Profile Image</Button>
-            </SaveLocation2>
+            
             <form  noValidate autoComplete="off">
-            <TextField></TextField>
+                <FirstNameLoc>
+                    <TextField id="outlined-basic" label="First Name" variant="outlined" fullWidth  />
+                </FirstNameLoc>
+                <LastNameLoc>
+                    <TextField id="outlined-basic" label="Last Name" variant="outlined" />
+                </LastNameLoc>
+                <UserIdLoc>
+                    <TextField id="outlined-basic" label="User ID" variant="outlined" fullWidth />
+                </UserIdLoc>
+                <PasswordLoc>
+                    <TextField id="outlined-basic" label="Password" variant="outlined" fullWidth />
+                </PasswordLoc>
+                <ConfirmPasswordLoc>
+                    <TextField id="outlined-basic" label="Confirm Password" variant="outlined" fullWidth />
+                </ConfirmPasswordLoc>
+                <EmailAddressLoc>
+                    <TextField id="outlined-basic" label="Email Address" variant="outlined" fullWidth />
+                </EmailAddressLoc>
+                <ConfirmLoc>
+                    <Button variant="contained" color="primary">Confirm</Button>
+                </ConfirmLoc>
+                <SaveLocation2>
+                    <Button variant="contained" color="secondary">Save Profile Image</Button>
+                </SaveLocation2>
+                <CommentLoc>Lorem Ipsum is simply dummy text of the printing and typesetting industry.  Lorem Ipsum has been the industry's standard dummy</CommentLoc>
             </form>
+            
         </Fix>
         
     )

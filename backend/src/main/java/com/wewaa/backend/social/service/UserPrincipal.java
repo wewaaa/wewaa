7fb4f6a4a8e0e85055e43f849d6e51cf6c3afc3c
kim1387 <kevink0@naver.com>
2022-01-10 00:34:59 +0900

    chore: add UserPrincipal.java for manage logined user

---
+++ backend/src/main/java/com/wewaa/backend/social/service/UserPrincipal.java
@@ -0,0 +1,92 @@
+package com.wewaa.backend.social.service;
+
+import com.wewaa.backend.social.model.type.ProviderType;
+import com.wewaa.backend.social.model.type.RoleType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class UserPrincipal implements OAuth2User, UserDetails, OidcUser {
+
+    private final String userId;
+    private final String password;
+    private final ProviderType providerType;
+    private final RoleType roleType;
+    private final Collection<GrantedAuthority> authorities;
+    private Map<String, Object> attributes;
+
+    // 사용자가 가지고 있는 토큰들
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    // 로그인 한 사용자가 가지고 있는 권한들
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return userId;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    // 차단된 계정 여부
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    // 자격이 유효한지
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    // 접근이 허용된 유저면 true 아니면 false
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
+    @Override
+    public Map<String, Object> getClaims() {
+        return null;
+    }
+
+    @Override
+    public OidcUserInfo getUserInfo() {
+        return null;
+    }
+
+    @Override
+    public OidcIdToken getIdToken() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}

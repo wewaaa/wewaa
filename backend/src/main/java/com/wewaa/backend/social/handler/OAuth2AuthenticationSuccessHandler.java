2cff36cdda47e8c9a4e8565e67c862e3ccd4a6fa
kim1387 <kevink0@naver.com>
2022-01-12 01:30:44 +0900

    refactor: delete else statement to cleancode

--- backend/src/main/java/com/wewaa/backend/social/handler/OAuth2AuthenticationSuccessHandler.java
+++ backend/src/main/java/com/wewaa/backend/social/handler/OAuth2AuthenticationSuccessHandler.java
@@ -138,11 +138,8 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
                 .anyMatch(authorizedRedirectUri -> {
                     // Only validate host and port. Let the clients use different paths if they want to
                     URI authorizedURI = URI.create(authorizedRedirectUri);
-                    if(authorizedURI.getHost().equalsIgnoreCase(clientRedirectUri.getHost())
-                            && authorizedURI.getPort() == clientRedirectUri.getPort()) {
-                        return true;
-                    }
-                    return false;
+                    return authorizedURI.getHost().equalsIgnoreCase(clientRedirectUri.getHost())
+                            && authorizedURI.getPort() == clientRedirectUri.getPort();
                 });
     }
 }

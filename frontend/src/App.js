f90686f9bc973494e7f90aaf7eaa29d1a16c68e3
Yujaebin <dbwpqls9998@naver.com>
2022-01-12 13:45:11 +0900

    feat: add Link(use Routes) function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { Route,Routes,Link} from 'react-router-dom';
 import UploadPage from  './pages/UploadPage';
 import SignPage from './pages/SignPage';
 import LoginPage from './pages/LoginPage';
 function App() {
   return (
-    <LoginPage/>
+    <div>
+      <Routes>
+        <Route exact path="/" element={<UploadPage/>}/>
+        <Route path="/Login" element={<LoginPage/>}/>
+        <Route path="/SignPage" element={<SignPage/>}/>
+      </Routes>
+    </div>
+    // exact true를통해 /와 /login or /signpage의 중복을 방지
   );
 }
 

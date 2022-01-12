1105703586acd8ec183fcb8e68e6ea39c6e10827
SeungmiNa <naissme0@gmail.com>
2022-01-11 23:49:59 -0800

    fix: chagne button style and resize background

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -4,6 +4,9 @@ import { useMediaQuery } from 'react-responsive';
 import AwesomeSlider from 'react-awesome-slider';
 import withAutoplay from 'react-awesome-slider/dist/autoplay';
 import 'react-awesome-slider/dist/styles.css';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+
 
 const MainPage = () => {
     const isDesktopOrLaptop = useMediaQuery({minDeviceWidth: 1224})
@@ -22,7 +25,7 @@ const MainPage = () => {
             <h1 class="WRITE">WRITE</h1>
             <h4 class="Explain">Lorem Ipsum is simply dummy text of the printing and 
             typesetting industry.  Lorem Ipsum has been the industry</h4>
-            <button class="ButtonDraw"> go to Draw </button>
+            <StyledButton variant="contained">go to Draw </StyledButton>
             <img className="HomeImage" alt="homeImage" src="/img/a.png" />
             
             </div>
@@ -43,17 +46,22 @@ const MainPage = () => {
 
 const AutoplaySlider = withAutoplay(AwesomeSlider);
 
-const slider = (
-  <AutoplaySlider
-    play={true}
-    cancelOnInteraction={false} // should stop playing on user interaction
-    interval={6000}
-  >
-    <div data-src="/img/Slide1.png" />
-    <div data-src="/img/Slide2.png" />
-    <div data-src="/img/Slide3.png" />
-  </AutoplaySlider>
-);
-
+const StyledButton = withStyles({
+    root: {
+      background: '#FF4D67',
+      borderRadius: 3,
+      border: 0,
+      color: 'white',
+      height: 48,
+      width: '286px',
+      padding: '0 30px',
+      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      left: '182px',
+      top: '530px',
+    },
+    label: {
+      textTransform: 'capitalize',
+    },
+  })(Button);
 
 export default MainPage

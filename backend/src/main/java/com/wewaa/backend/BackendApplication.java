dff82f645afc8d907e71cb326956c0e26ae2760f
kim1387 <kevink0@naver.com>
2022-01-12 22:44:44 +0900

    fix: add properties setting for cors at Springboot main

--- backend/src/main/java/com/wewaa/backend/BackendApplication.java
+++ backend/src/main/java/com/wewaa/backend/BackendApplication.java
@@ -1,5 +1,6 @@
 package com.wewaa.backend;
 
+import com.wewaa.backend.commons.config.properties.CorsProperties;
 import com.wewaa.backend.commons.config.properties.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 
 @SpringBootApplication
 @EnableConfigurationProperties({
+        CorsProperties.class,
         AppProperties.class
 })
 public class BackendApplication {

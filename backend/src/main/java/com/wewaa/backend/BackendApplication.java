471525f1b35594bcd8ae08ed1d9d5bf0d9ccaf43
kim1387 <kevink0@naver.com>
2022-01-17 13:35:19 +0900

    chore: Disabled spring security

--- backend/src/main/java/com/wewaa/backend/BackendApplication.java
+++ backend/src/main/java/com/wewaa/backend/BackendApplication.java
@@ -1,7 +1,7 @@
 package com.wewaa.backend;
 
-import com.wewaa.backend.commons.config.properties.CorsProperties;
 import com.wewaa.backend.commons.config.properties.AppProperties;
+import com.wewaa.backend.commons.config.properties.CorsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
--- backend/src/main/java/com/wewaa/backend/social/api/UserController.java
+++ backend/src/main/java/com/wewaa/backend/social/api/UserController.java
@@ -1,40 +1,40 @@
-package com.wewaa.backend.social.api;
-
-import com.wewaa.backend.commons.utils.ApiResponseUtill;
-import com.wewaa.backend.social.model.entity.User;
-import com.wewaa.backend.social.service.UserService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/v1/users")
-@RequiredArgsConstructor
-public class UserController {
-
-    private final UserService userService;
-    @Operation(summary = "test hello", description = "hello api example")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "OK !!"),
-            @ApiResponse(responseCode = "400", description = "BAD REQUEST !!"),
-            @ApiResponse(responseCode = "404", description = "NOT FOUND !!"),
-            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!")
-    })
-    @GetMapping
-    public ApiResponseUtill getUser() {
-        org.springframework.security.core.userdetails.User principal
-                = (org.springframework.security.core.userdetails.User) SecurityContextHolder
-                    .getContext()
-                    .getAuthentication()
-                    .getPrincipal();
-
-        User user = userService.getUser(principal.getUsername());
-
-        return ApiResponseUtill.success("user", user);
-    }
-}
+//package com.wewaa.backend.social.api;
+//
+//import com.wewaa.backend.commons.utils.ApiResponseUtill;
+//import com.wewaa.backend.social.model.entity.User;
+//import com.wewaa.backend.social.service.UserService;
+//import io.swagger.v3.oas.annotations.Operation;
+//import io.swagger.v3.oas.annotations.responses.ApiResponse;
+//import io.swagger.v3.oas.annotations.responses.ApiResponses;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/api/v1/users")
+//@RequiredArgsConstructor
+//public class UserController {
+//
+//    private final UserService userService;
+//    @Operation(summary = "test hello", description = "hello api example")
+//    @ApiResponses({
+//            @ApiResponse(responseCode = "200", description = "OK !!"),
+//            @ApiResponse(responseCode = "400", description = "BAD REQUEST !!"),
+//            @ApiResponse(responseCode = "404", description = "NOT FOUND !!"),
+//            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!")
+//    })
+//    @GetMapping
+//    public ApiResponseUtill getUser() {
+//        org.springframework.security.core.userdetails.User principal
+//                = (org.springframework.security.core.userdetails.User) SecurityContextHolder
+//                    .getContext()
+//                    .getAuthentication()
+//                    .getPrincipal();
+//
+//        User user = userService.getUser(principal.getUsername());
+//
+//        return ApiResponseUtill.success("user", user);
+//    }
+//}

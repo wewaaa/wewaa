7292a7ffb557518111cbadaf9b8facc578569eb1
Yujaebin <dbwpqls9998@naver.com>
2022-01-20 15:48:39 +0900

    fix

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -83,10 +83,10 @@ function Modal({closeModal}) {
                     <button onClick= {()=> closeModal(false)}> X </button>
                     <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
                         {imagesUrl.map((image) => (
-                            <ImageListItem key={image}>
+                            <ImageListItem key={image.images_url}>
                             <img
-                                src={`${image}?w=164&h=164&fit=crop&auto=format`}
-                                srcSet={`${image}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                                src={`${image.images_url}?w=164&h=164&fit=crop&auto=format`}
+                                srcSet={`${image.images_url}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
                                 loading="lazy"
                                 alt="profile"
                             />
